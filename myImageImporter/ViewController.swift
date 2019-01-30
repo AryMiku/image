38113bd9bbb113f5d573b7ea4c78d1a1453af410
Admin <admin@MAC12.local>
2019-01-30 13:16:10 +0700

    Initial Commit

---
+++ myImageImporter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myImageImporter
+//
+//  Created by Admin on 30/1/2562 BE.
+//  Copyright © 2562 AryMiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
